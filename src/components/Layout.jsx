994a739c2e083a4c2e998559ac8b98cbcb25e1fd
allanbfr77 <alan.tcn1@gmail.com>
2026-06-22 14:50:49 -0300

    hashes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Background from './Background';
 import Nav from './Nav';
 import Ticker from './Ticker';
 import Footer from './Footer';
 
 export default function Layout() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <Background />
